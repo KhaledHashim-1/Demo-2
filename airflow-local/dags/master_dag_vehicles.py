174440120719c0a8939b5fe008702518e284aed5
Khaled <khaledhashim96@gmail.com>
2025-09-29 07:46:18 +0000

    Changed master dag to have parallelism

--- airflow-local/dags/master_dag_vehicles.py
+++ airflow-local/dags/master_dag_vehicles.py
@@ -4,31 +4,34 @@ from datetime import datetime
 
 default_args = {"retries": 0}
 
-PIPELINE_ORDER = [
-    "el-vehicle_price_prediction",
-    "dbt-vehicle_price_prediction",
-]
+PIPELINE_GROUPS = {
+    "Group1": [
+        "el-vehicle_price_prediction",
+        "dbt-vehicle_price_prediction",
+    ],
+}
 
 with DAG(
-    dag_id="controller_master_khaled",
+    dag_id="controller_master_khaled_parallel",
     default_args=default_args,
-    description="Master DAG in order",
-    schedule_interval=None,  
+    description="Master DAG with parallel pipeline execution",
+    schedule_interval=None,
     start_date=datetime(2025, 9, 1),
     catchup=False,
 ) as dag:
 
-    previous_task = None
-    for dag_id in PIPELINE_ORDER:
-        task = TriggerDagRunOperator(
-            task_id=f"trigger_{dag_id}",
-            trigger_dag_id=dag_id,
-            reset_dag_run=True,
-            wait_for_completion=True,
-            poke_interval=10,
-        )
+    for group_name, dags in PIPELINE_GROUPS.items():
+        previous_task = None
+        for dag_id in dags:
+            task = TriggerDagRunOperator(
+                task_id=f"trigger_{dag_id}",
+                trigger_dag_id=dag_id,
+                reset_dag_run=True,
+                wait_for_completion=True,
+                poke_interval=10,
+            )
 
-        if previous_task:
-            previous_task >> task
+            if previous_task:
+                previous_task >> task  # chain inside group
 
-        previous_task = task
+            previous_task = task
